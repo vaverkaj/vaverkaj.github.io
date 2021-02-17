f41669aeb5a722e5c1ee5dd8489d5987ac11488a
Jakub Váverka <jakub.vaverka@email.cz>
2021-02-17 20:52:55 +0100

    Dark theme, code font

---
+++ main.js
@@ -0,0 +1,211 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ 0:
+/*!***************************!*\
+  !*** multi ./src/main.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\Users\CEN67002\Documents\vaverkaj.github.io\pages\src\main.ts */"zUnb");
+
+
+/***/ }),
+
+/***/ "AytR":
+/*!*****************************************!*\
+  !*** ./src/environments/environment.ts ***!
+  \*****************************************/
+/*! exports provided: environment */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+const environment = {
+    production: false
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+/***/ }),
+
+/***/ "Sy1n":
+/*!**********************************!*\
+  !*** ./src/app/app.component.ts ***!
+  \**********************************/
+/*! exports provided: AppComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/router */ "tyNb");
+
+
+class AppComponent {
+    constructor() {
+        this.title = 'pages';
+    }
+}
+AppComponent.ɵfac = function AppComponent_Factory(t) { return new (t || AppComponent)(); };
+AppComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: AppComponent, selectors: [["app-root"]], decls: 25, vars: 0, template: function AppComponent_Template(rf, ctx) { if (rf & 1) {
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "h1");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, " Uvid\u011Bt tak bal\u00F3nky n\u00E1hodn\u00FD kolemjdouc\u00ED\n");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](2, "p");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](3, " Jist\u011B by si pomyslel, \u017Ee u\u017E tu takhle poletuj\u00ED snad tis\u00EDc let. ]St\u00E1le si v\u00EDcem\u00E9n\u011B dr\u017E\u00ED v\u00FD\u0161ku a ani do stran se p\u0159\u00EDli\u0161 nepohybuj\u00ED. Proti slunci to vypad\u00E1, \u017Ee se slunce pohybuje k z\u00E1padu rychleji ne\u017E bal\u00F3nky, a mo\u017En\u00E1 to tak skute\u010Dn\u011B je. Nejeden filozof by mohl tvrdit, \u017Ee bal\u00F3nky se sluncem z\u00E1vod\u00ED, ale fyzikov\u00E9 by to jist\u011B vyvr\u00E1tili. Z fyzik\u00E1ln\u00EDho pohledu toti\u017E bal\u00F3nky p\u016Fsob\u00ED zcela nezaj\u00EDmav\u011B. Nejv\u00EDc bezpochyby zaujmou d\u011Bti - jedna mal\u00E1 hol\u010Di\u010Dka zrovna v\u010Dera div nebre\u010Dela, \u017Ee by snad bal\u00F3nky mohly prasknout. ");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](4, "h2");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](5, " A co teprve ta stuha.\n");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](6, "p");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](7, " Stuha, kterou je ka\u017Ed\u00FD z trojice bal\u00F3nk\u016F uv\u00E1z\u00E1n, aby se nevypustil. O\u010Dividn\u011B je uv\u00E1zan\u00E1 dostate\u010Dn\u011B pevn\u011B, proto\u017Ee bal\u00F3nky skute\u010Dn\u011B neuch\u00E1zej\u00ED. To ale nen\u00ED nic zvl\u00E1\u0161tn\u00EDho. P\u0159ekvapit by v\u0161ak mohl fakt, \u017Ee nikdo, snad krom toho, kdo bal\u00F3nky k obloze vypustil, netu\u0161\u00ED, jakou m\u00E1 ona stuha barvu. ");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](8, "\nJe toti\u017E tak leskl\u00E1, \u017Ee za sv\u011Btla se v n\u00ED odr\u00E1\u017E\u00ED nebe a za tmy zase nen\u00ED vid\u011Bt v\u016Fbec. Kdy\u017E sv\u00EDt\u00ED slunce tak siln\u011B jako nyn\u00ED, tak se stuha t\u0159pyt\u00ED jako kapka rosy a jen m\u00E1lokdo vydr\u017E\u00ED d\u00EDvat se na ni p\u0159\u00EDmo d\u00E9le ne\u017E p\u00E1r chvil. Jak vlastn\u011B vypadaj\u00ED ony bal\u00F3nky?. Ptaj\u00ED se \u010Dasto lid\u00E9.Inu jak by vypadaly - jako b\u011B\u017En\u00E9 pou\u0165ov\u00E9 bal\u00F3nky st\u0159edn\u00ED velikosti, tak akor\u00E1t nafouknut\u00E9. ");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](9, "h2");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](10, " \u010Cerven\u00FD se vedle modr\u00E9ho a zelen\u00E9ho zd\u00E1 trochu men\u0161\u00ED, ale to je nejsp\u00ED\u0161 jen optick\u00FD klam\n");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](11, "h3");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](12, " V\u00EDtr skoro nefouk\u00E1 a tak by se na prvn\u00ED pohled mohlo zd\u00E1t\n");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](13, "h4");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](14, " Bbal\u00F3nky snad v\u016Fbec nepohybuj\u00ED\n");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](15, "h5");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](16, " Jeliko\u017E slunce jasn\u011B z\u00E1\u0159\u00ED\n");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](17, "p");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](18, " Bal\u00F3nky p\u016Fsob\u00ED jako jak\u00E1si fata morg\u00E1na uprost\u0159ed pou\u0161t\u011B. Zkr\u00E1tka \u0161iroko daleko nikde nic, jen zelenkav\u00E1 tr\u00E1va, jasn\u011B modr\u00E1 obloha a t\u0159i k\u0159iklav\u011B barevn\u00E9 pou\u0165ov\u00E9 bal\u00F3nky, kter\u00E9 se t\u00E9m\u011B\u0159 nepozorovateln\u011B pohupuj\u00ED ani ne moc vysoko, ani moc n\u00EDzko nad zem\u00ED. Kdyby pod bal\u00F3nky nebyla syt\u011B zelenkav\u00E1 tr\u00E1va, ale t\u0159eba such\u00E1 silnice \u010Di beton, mo\u017En\u00E1 by bylo vid\u011Bt jejich barevn\u00E9 st\u00EDny - to jak p\u0159es polopr\u016Fsvitn\u00E9 barevn\u00E9 bal\u00F3nky proch\u00E1z\u00ED ostr\u00E9 slune\u010Dn\u00ED paprsky.Jen\u017Ee kv\u016Fli v\u0161udy p\u0159\u00EDtomn\u00E9 tr\u00E1v\u011B jsou st\u00EDny bal\u00F3nk\u016F sotva vid\u011Bt, nato\u017E aby \u0161lo rozeznat, jakou barvu tyto st\u00EDny maj\u00ED. Uvid\u011Bt tak bal\u00F3nky n\u00E1hodn\u00FD kolemjdouc\u00ED, jist\u011B by si pomyslel, \u017Ee u\u017E tu takhle poletuj\u00ED snad tis\u00EDc let. St\u00E1le si v\u00EDcem\u00E9n\u011B dr\u017E\u00ED v\u00FD\u0161ku a ani do stran se p\u0159\u00EDli\u0161 nepohybuj\u00ED. Proti slunci to vypad\u00E1, \u017Ee se slunce pohybuje k z\u00E1padu rychleji ne\u017E b al\u00F3nky, a mo\u017En\u00E1 to tak skute\u010Dn\u011B je. Nejeden filozof by mohl tvrdit, \u017Ee bal\u00F3nky se sluncem z\u00E1vod\u00ED, ale fyzikov\u00E9 by to jist\u011B vyvr\u00E1tili. Z fyzik\u00E1ln\u00EDho pohledu toti\u017E bal\u00F3nky p\u016Fsob\u00ED zcela nezaj\u00EDmav\u011B. Nejv\u00EDc bezpochyby zaujmou d\u011Bti - jedna mal\u00E1 hol\u010Di\u010Dka zrovna v\u010Dera div nebre\u010Dela, \u017Ee by snad bal\u00F3nky mohly prasknout. A co teprve ta stuha.Stuha, kterou je ka\u017Ed\u00FD z trojice bal\u00F3nk\u016F uv\u00E1z\u00E1n, aby se nevypustil. O\u010Dividn\u011B je uv\u00E1zan\u00E1 dostate\u010Dn\u011B pevn\u011B, proto\u017Ee bal\u00F3nky skute\u010Dn\u011B neuch\u00E1zej\u00ED. To ale nen\u00ED nic zvl\u00E1\u0161tn\u00EDho. P\u0159ekvapit by v\u0161ak mohl fakt, \u017Ee nikdo, snad krom toho, kdo bal\u00F3nky k obloze vypustil, netu\u0161\u00ED, jakou m\u00E1 ona stuha barvu. Je toti\u017E tak leskl\u00E1, \u017Ee za sv\u011Btla se v n\u00ED odr\u00E1\u017E\u00ED nebe a za tmy zase nen\u00ED vid\u011Bt v\u016Fbec. Kdy\u017E sv\u00EDt\u00ED slunce tak siln\u011B jako nyn\u00ED, tak se stuha t\u0159pyt\u00ED jako kapka rosy a jen m\u00E1lokdo vydr\u017E\u00ED d\u00EDvat se na ni p\u0159\u00EDmo d\u00E9le ne\u017E p\u00E1r chvil. Jak vlastn\u011B vypadaj\u00ED ony bal\u00F3nky?.Ptaj\u00ED ");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](19, "b");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](20, "se \u010Dasto lid\u00E9");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](21, ". ");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](22, "strong");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](23, " Inu jak by vypadaly - jako b\u011B\u017En\u00E9 pou\u0165ov\u00E9 bal\u00F3nky st\u0159edn\u00ED velikosti, tak akor\u00E1t nafouknut\u00E9. \u010Cerven\u00FD se vedle modr\u00E9ho a zelen\u00E9ho zd\u00E1 trochu men\u0161\u00ED, ale to je nejsp\u00ED\u0161 jen optick\u00FD klam, a i kdyby byl skute\u010Dn\u011B o n\u011Bco m\u00E1lo men\u0161\u00ED, tak v\u00E1\u017En\u011B jen o tro\u0161i\u010Dku. V\u00EDtr skoro nefouk\u00E1 a tak by se na prvn\u00ED pohled mohlo zd\u00E1t, \u017Ee se bal\u00F3nky snad v\u016Fbec nepohybuj\u00ED. Jenom tak klidn\u011B levituj\u00ED ve vzduchu. ");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](24, "router-outlet");
+    } }, directives: [_angular_router__WEBPACK_IMPORTED_MODULE_1__["RouterOutlet"]], styles: ["\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJhcHAuY29tcG9uZW50LnNjc3MifQ== */"] });
+
+
+/***/ }),
+
+/***/ "ZAI4":
+/*!*******************************!*\
+  !*** ./src/app/app.module.ts ***!
+  \*******************************/
+/*! exports provided: AppModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "jhN1");
+/* harmony import */ var _app_routing_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./app-routing.module */ "vY5A");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app.component */ "Sy1n");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+
+
+class AppModule {
+}
+AppModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineNgModule"]({ type: AppModule, bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]] });
+AppModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineInjector"]({ factory: function AppModule_Factory(t) { return new (t || AppModule)(); }, providers: [], imports: [[
+            _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
+            _app_routing_module__WEBPACK_IMPORTED_MODULE_1__["AppRoutingModule"]
+        ]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵsetNgModuleScope"](AppModule, { declarations: [_app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]], imports: [_angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
+        _app_routing_module__WEBPACK_IMPORTED_MODULE_1__["AppRoutingModule"]] }); })();
+
+
+/***/ }),
+
+/***/ "vY5A":
+/*!***************************************!*\
+  !*** ./src/app/app-routing.module.ts ***!
+  \***************************************/
+/*! exports provided: AppRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppRoutingModule", function() { return AppRoutingModule; });
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/router */ "tyNb");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+
+const routes = [];
+class AppRoutingModule {
+}
+AppRoutingModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineNgModule"]({ type: AppRoutingModule });
+AppRoutingModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineInjector"]({ factory: function AppRoutingModule_Factory(t) { return new (t || AppRoutingModule)(); }, imports: [[_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"].forRoot(routes)], _angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵsetNgModuleScope"](AppRoutingModule, { imports: [_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]], exports: [_angular_router__WEBPACK_IMPORTED_MODULE_0__["RouterModule"]] }); })();
+
+
+/***/ }),
+
+/***/ "zUnb":
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "jhN1");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "ZAI4");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "AytR");
+
+
+
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["enableProdMode"])();
+}
+_angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["platformBrowser"]().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+    .catch(err => console.error(err));
+
+
+/***/ }),
+
+/***/ "zn8P":
+/*!******************************************************!*\
+  !*** ./$$_lazy_route_resource lazy namespace object ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "zn8P";
+
+/***/ })
+
+},[[0,"runtime","vendor"]]]);
+//# sourceMappingURL=main.js.map
