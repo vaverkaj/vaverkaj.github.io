7fdd96a139ed4629fe5b96e93ae6e7e03d2e50f7
Jakub Váverka <jakub.vaverka@email.cz>
2021-02-18 16:05:17 +0100

    Debug rectangle

--- pages/src/app/directives/block-comment.directive.ts
+++ pages/src/app/directives/block-comment.directive.ts
@@ -4,7 +4,7 @@ import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular
   selector: '[appBlockComment]'
 })
 export class BlockCommentDirective implements OnInit{
-  context: any;
+  canvas: any;
   originalText: string;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
@@ -13,30 +13,34 @@ export class BlockCommentDirective implements OnInit{
   ngOnInit(): void {
     this.initCanvas(); 
     this.renderer.addClass(this.el.nativeElement,'c-mono-3');
-    this.originalText = this.el.nativeElement.innerHTML;
+    this.originalText = this.el.nativeElement.innerText;
     this.drawDecoration();
+    this.drawDebugRect();
   }
   
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.drawDecoration();
+    this.drawDebugRect();
   }
 
   private initCanvas() {
-    let canvas = this.renderer.createElement('canvas');
-    this.context = canvas.getContext('2d');
-    this.context.font = window.getComputedStyle(this.el.nativeElement).getPropertyValue('font');
+    this.canvas = this.renderer.createElement('canvas');
+    const context = this.canvas.getContext('2d');
+    this.renderer.appendChild(this.el.nativeElement.parentElement, this.canvas);
+    context.font = window.getComputedStyle(this.el.nativeElement).getPropertyValue('font');
   }
 
   private insertAtStartOfEachLine(text, elementWidth, value): string {
-    elementWidth -= this.context.measureText('\u00A0*\u00A0').width;
+    const context = this.canvas.getContext('2d');
+    elementWidth -= context.measureText('\u00A0*\u00A0').width;
     text = text.trim();
     let newText = '';
     let spaceLeft = elementWidth;
-    const spaceWidth = this.context.measureText(' ').width;
+    const spaceWidth = context.measureText(' ').width;
     const words = text.split(' ');
     words.forEach(element => {
-      const wordWidth = this.context.measureText(element).width;
+      const wordWidth = context.measureText(element).width;
       if(wordWidth + spaceWidth > spaceLeft) {
         newText += '<span class="noselect">'+value+'</span>';
         spaceLeft = elementWidth - wordWidth;
@@ -49,12 +53,20 @@ export class BlockCommentDirective implements OnInit{
     return newText;
   }
 
-  private drawDecoration() 
-  {
+  private drawDecoration() {
     const elementRect = this.el.nativeElement.getBoundingClientRect();
-    console.log(elementRect);
     let newText = this.insertAtStartOfEachLine(this.originalText, elementRect.width, '</br>\u00A0*\u00A0');
     newText = '<span class="noselect">/**</br>\u00A0*\u00A0</span>' + newText + '<span class="noselect"></br>\u00A0*/</br></span>'
     this.renderer.setProperty(this.el.nativeElement, 'innerHTML', newText);
+  }
+
+  private drawDebugRect() {
+    const bounds = this.el.nativeElement.getBoundingClientRect();
+    this.renderer.setStyle(this.canvas, 'position', 'absolute');
+    this.renderer.setStyle(this.canvas, 'left', bounds.left +'px');
+    this.renderer.setStyle(this.canvas, 'top', bounds.top +'px');
+    this.renderer.setStyle(this.canvas, 'width', bounds.width +'px');
+    this.renderer.setStyle(this.canvas, 'height', bounds.height +'px');
+    this.renderer.setStyle(this.canvas, 'background-color', 'rgba(255, 0, 0, 0.2)');
   }
 }
