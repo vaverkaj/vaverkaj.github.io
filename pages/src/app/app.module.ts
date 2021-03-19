e5f2adb61def27ac2d1ff910a4d3903afef0248f
jakub.vaverka@email.cz <jakub.vaverka@email.cz>
2021-03-19 22:23:12 +0100

    Navigation

--- pages/src/app/app.module.ts
+++ pages/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlockCommentComponent } from './components/block-comment/block-comment.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlockCommentComponent
+    BlockCommentComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
